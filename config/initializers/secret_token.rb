f30a1e61c9a98871ace2da9937efdc6a289d159b
Todd Squitieri <todd.squitieri@alumni.newschool.edu>
2015-08-27 12:58:18 +0000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+WheresMyGirlfriend::Application.config.secret_key_base = '786871010b6aa6eb704b2d2d3475afd7f2f6dadae3b680e4213c3b769d6d677b166a949c042d7479cfccb19c51940fae7f23344c39c239a0f2605d384aa8937d'
