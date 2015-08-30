89495139a39b902646ebed62fc018e1ce8373ea8
Todd Squitieri <todd.squitieri@alumni.newschool.edu>
2015-08-30 08:26:39 +0000

    Added the devise gem

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,4 +1,5 @@
 WheresMyGirlfriend::Application.configure do
+  config.action_mailer.default_url_options = { :host => 'wheres-my-girlfriend.herokuapp.com' }
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
